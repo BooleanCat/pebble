992f5bf132c626e35748464d145a98da3416e429
BooleanCat <thgodkin@gmail.com>
2020-03-27 08:54:48 +0000

    richer error info

--- libpebble/src/lib.rs
+++ libpebble/src/lib.rs
@@ -27,23 +27,28 @@ pub enum Error {
     NoSuchContainer,
 
     #[snafu(display("create directory: {}", "source"))]
-    CreateDirectory { source: io::Error },
+    CreateDirectory { source: io::Error, path: String },
 
     #[snafu(display("change owner: {}", "source"))]
-    ChangeOwner { source: nix::Error },
+    ChangeOwner { source: nix::Error, path: String },
 }
 
 pub fn setup() -> Result<(), Error> {
-    if let Err(err) = fs::create_dir("/run/pebble") {
-        if err.kind() != io::ErrorKind::AlreadyExists {
-            return Err(Error::CreateDirectory { source: err });
+    if let Err(source) = fs::create_dir("/run/pebble") {
+        if source.kind() != io::ErrorKind::AlreadyExists {
+            return Err(Error::CreateDirectory {
+                source: source,
+                path: String::from("/run/pebble"),
+            });
         }
     }
 
     let uid = unistd::Uid::from_raw(5000);
     let gid = unistd::Gid::from_raw(5000);
 
-    unistd::chown("/run/pebble", Some(uid), Some(gid)).context(ChangeOwner {})?;
+    unistd::chown("/run/pebble", Some(uid), Some(gid)).context(ChangeOwner {
+        path: String::from("/run/pebble"),
+    })?;
 
     Ok(())
 }
